c28902e98111fbb1573daf23fe19e0ee166d962e
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-17 14:19:38 +0200

    prepare for checking if in production or development

--- webpack.config.js
+++ webpack.config.js
@@ -3,33 +3,36 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPLugin = require('extract-text-webpack-plugin');
 
+const useDevServer = false;
+const publicPath = useDevServer ? 'http://localhost:8080/build/' : '/build/';
+const isProduction = (process.env.NODE_ENV === 'production');
+const useSourceMaps= !isProduction;
+
 const styleLoader = {
     loader: 'style-loader',
     options: {
-        sourceMap: true
+        sourceMap: useSourceMaps,
     }
 };
 const cssLoader = {
     loader: 'css-loader',
     options: {
-        sourceMap: true
-    }
+        sourceMap: useSourceMaps,
+    },
 };
 const sassLoader = {
     loader: 'sass-loader',
     options: {
-        sourceMap: true
-    }
+        sourceMap: true,
+    },
 };
 const resolveUrlLoader = {
     loader: 'resolve-url-loader',
     options: {
-        sourceMap: true
-    }
+        sourceMap: useSourceMaps,
+    },
 };
 
-const useDevServer = false;
-const publicPath = useDevServer ? 'http://localhost:8080/build/' : '/build/';
 
 const webpackConfig = {
     entry: {
@@ -61,6 +64,7 @@ const webpackConfig = {
                 use: ExtractTextPLugin.extract({
                     use: [cssLoader],
                     fallback: styleLoader,
+                    minimize: isProduction,
                 }),
                 
             },
@@ -114,14 +118,14 @@ const webpackConfig = {
         new webpack.optimize.CommonsChunkPlugin({
             name: [
                 'layout',
-                'manifest'
+                'manifest',
             ],
             minChunks: Infinity,
         }),
         new ExtractTextPLugin('[name].css'),
 
     ],
-    devtool: 'inline-source-map',
+    devtool: useSourceMaps ? 'inline-source-map' : false,
     devServer: {
         contentBase: './web',
         headers: {
@@ -131,9 +135,25 @@ const webpackConfig = {
 };
 
 console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'production') {
+if (isProduction) {
+    
     webpackConfig.plugins.push(
         new webpack.optimize.UglifyJsPlugin()
     );
+    
+    webpackConfig.plugins.push(
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false,
+        })
+    );
+
+    webpackConfig.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': JSON.stringify('production')
+        })
+    );
+
 }
+
 module.exports = webpackConfig;
