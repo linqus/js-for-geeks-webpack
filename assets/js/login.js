481c7d39d06ccdc5c2900425902cc2e89a635799
Karol Linkiewicz <karol@linkiewicz.pl>
2023-04-17 20:15:01 +0200

    code splitting, so js and css are loaded on request

--- assets/js/login.js
+++ assets/js/login.js
@@ -15,7 +15,10 @@ $('.js-login-field-username').on('keydown', function(e) {
     $('.login-long-username-warning').remove();
 
     if ($usernameInput.val().length >= 20) {
-        const $warning = $('<div class="login-long-username-warning">This is a really long username - are you sure that is right?</div>');
-        $usernameInput.before($warning);
+        import('./Components/username_validation_error').then(username_validation_error => {
+            console.log(username_validation_error);
+            username_validation_error.default($usernameInput); ;
+        })
+
     }
 });
